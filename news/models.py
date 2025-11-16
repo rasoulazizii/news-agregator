f5c24fbaa017485abb4c4e63d7f7c2c3f2395f7d
Rasoul Azizi <rasoulazizi@outlook.com>
2025-11-16 23:46:27 +0330

    feat: newsource model created

--- news/models.py
+++ news/models.py
@@ -4,3 +4,11 @@ class Category(models.Model):
     name = models.CharField(max_length=220)
     slug = models.SlugField()
 
+class NewSource(models.Model):
+    name = models.CharField(max_length=220)
+    website_url = models.URLField()
+    rss_feed_url = models.URLField()
+    is_active = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
