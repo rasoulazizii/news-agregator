29043a84558c0e8c873fb4742cd3c84857d3d06f
Rasoul Azizi <rasoulazizi@outlook.com>
2025-11-16 23:43:10 +0330

    feat: Profile models updated

--- users/models.py
+++ users/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from news.models import Category
 
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+    interested_categories = models.ManyToManyField(
+        Category
+    )
