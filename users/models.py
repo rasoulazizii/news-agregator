f2ee540bc6dc88cf7c7b2ce93b2211cac2f1ac18
Rasoul Azizi <rasoulazizi@outlook.com>
2025-11-17 12:09:34 +0330

    feat: add custom user model

--- users/models.py
+++ users/models.py
@@ -1,9 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 from news.models import Category
 
+class CustomUser(AbstractUser):
+    pass
+
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     interested_categories = models.ManyToManyField(
         Category
     )
