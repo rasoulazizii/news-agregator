781f69e844a6644a1f1398b53f90dbb5f0112ff3
Rasoul Azizi <rasoulazizi@outlook.com>
2025-11-17 12:14:46 +0330

    feat: add simple admin panel

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, NewsSource, Article
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(NewsSource)
+admin.site.register(Article)
+
+
