2247c4dfc1202784b0f2d9dd3074640590fe8095
Rasoul Azizi <rasoulazizi@outlook.com>
2025-11-17 12:27:11 +0330

    feat: celery and redis config set

--- config/settings.py
+++ config/settings.py
@@ -144,3 +144,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# celery 
+CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/1"
+
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
