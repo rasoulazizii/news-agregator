48f6c490932d4db3e397808bce7dfdc6012c4f78
Rasoul Azizi <rasoulazizi@outlook.com>
2025-11-18 21:11:07 +0330

    feat: news admin updated

--- news/admin.py
+++ news/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from .models import Category, NewsSource, Article
 
-admin.site.register(Category)
-admin.site.register(NewsSource)
-admin.site.register(Article)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)} 
 
+@admin.register(NewsSource)
+class NewsSourceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'website_url', 'is_active')
+    list_filter = ('is_active', 'category')
+    search_fields = ('name', 'website_url')
 
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'source', 'published_date')
+    list_filter = ('source', 'published_date')
+    search_fields = ('title', 'description')
+    def has_add_permission(self, request):
+        return False
