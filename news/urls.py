ff35b6d580194becc5dc3bbfbba199528bcb1dbd
Rasoul Azizi <rasoulazizi@outlook.com>
2025-11-20 20:12:25 +0330

    feat(news): add feed system

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CategoryListView, ArticleListView, ArticleDetailView
+from .views import CategoryListView, ArticleListView, ArticleDetailView, PersonalizedFeedView
 
 urlpatterns = [
     path('categories/', CategoryListView.as_view(), name='category-list'),
     path('articles/', ArticleListView.as_view(), name='article-list'),
     path('articles/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
+    path('feed/', PersonalizedFeedView.as_view(), name='personalized-feed'),
 ]
--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from .models import Category, Article
 from .serializers import CategorySerializer, ArticleSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class CategoryListView(generics.ListAPIView):
     queryset = Category.objects.all()
@@ -17,3 +18,21 @@ class ArticleDetailView(generics.RetrieveAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     permission_classes = (AllowAny,)
+
+
+class PersonalizedFeedView(generics.ListAPIView):
+    serializer_class = ArticleSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user_profile = self.request.user.profile
+
+        interested_categories = user_profile.interested_categories.all()
+        if not interested_categories.exists():
+            return Article.objects.none()
+
+        queryset = Article.objects.filter(
+            source__category__in=interested_categories
+        ).order_by('-published_date')
+
+        return queryset
